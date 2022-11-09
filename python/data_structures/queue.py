94daca46d6373c9cb92c55bff5446ffa194340bd
Natalija Germek <n.germek@icloud.com>
2022-11-09 01:38:00 -0800

    passed more queue tests.

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,3 +1,6 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
+
 class Queue:
     """
     enqueue(value)
@@ -7,6 +10,23 @@ class Queue:
    rear.next <-- node
    rear <-- node
 
+    dequeue()
+    // INPUT <-- none
+    // OUTPUT <-- value of the removed Node
+    // EXCEPTION if queue is empty
+
+   Node temp <-- front
+   front <-- front.next
+   temp.next <-- null
+
+   return temp.value
+
+    peek()
+    // INPUT <-- none
+    // OUTPUT <-- value of the front Node in Queue
+    // EXCEPTION if Queue is empty
+
+   return front.value
 
     """
 
@@ -20,15 +40,46 @@ class Queue:
         pass
 
     def traverse(self):
-        current = self.head
+        current = self.front
         while current.next is not None:
             current = current.next
         return current
 
     def enqueue(self, value):
-        while self.next is not None:
-            node.traverse():
-        self.next = Node(value)
+        if not self.front:
+            self.front = Node(value)
+        else:
+            while self.next is not None:
+                current = self.front
+                node = Node(value)
+                final_node = self.traverse()
+                current = final_node
+                current.next = node
+
+    # def pop(self):
+    #     # temp = self.top
+    #     # top = Node(value)
+    #     if not self.top:
+    #         raise InvalidOperationError(Exception("Method not allowed on empty collection"))
+    #     temp = self.top
+    #     self.top = self.top.next
+    #     return temp.value
+    def dequeue(self):
+        if self.front is None:
+            raise InvalidOperationError(Exception())
+        elif self.front is not None:
+            temp = self.front
+            temp.next = None
+            return temp.value
+
+    def peek(self):
+        pass
+
+    def is_empty(self):
+        if self.front is None:
+            return True
+        else:
+            return False
 
 
 class Node:
--- python/tests/data_structures/test_queue.py
+++ python/tests/data_structures/test_queue.py
@@ -7,7 +7,7 @@ def test_exists():
     assert Queue
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_enqueue():
     q = Queue()
     q.enqueue("apple")
@@ -16,7 +16,7 @@ def test_enqueue():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_dequeue():
     q = Queue()
     q.enqueue("apple")
@@ -63,7 +63,7 @@ def test_enqueue_two():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_dequeue_when_empty():
     q = Queue()
     with pytest.raises(InvalidOperationError):
@@ -91,7 +91,7 @@ def test_peek_post_dequeue():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+# @pytest.mark.skip("TODO")
 def test_is_empty():
     q = Queue()
     actual = q.is_empty()
