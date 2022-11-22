8845f0f6fe40db56ec8e64345327cc04e12cb119
Natalija Germek <n.germek@icloud.com>
2022-11-21 23:18:27 -0800

    still working on completing contains and add method of binary search tree

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -3,30 +3,28 @@ from data_structures.binary_tree import BinaryTree, Node
 
 class BinarySearchTree(BinaryTree):
 
-    def __init__(self, root=None):
-        self.root = root
-
-    def add(self, values):
+    def add(self, values=[]):
         new_value = Node(values)
 
         if self.root is None:
             self.root = new_value
             current = self.root
             if values is not None or type(values) == str:
-                return "Node must be equal to interger"
+                return "Node must be equal to integer"
 
             while current is not None:
                 if current.values > values:
-                    if current.left is None:
+                    if current.left is not None:
+                    # values.append
                         current.left = new_value
                     else:
                         current = current.left
 
-                elif current.values < values:
-                    if current.right is None:
-                        current.right = new_value
-                    else:
-                        current = current.right
+                # elif current.values < values:
+                #     if current.right is None:
+                #         current.right = new_value
+                #     else:
+                #         current = current.right
 
                 if current.values == values:
                     return "Value already exists in the tree"
