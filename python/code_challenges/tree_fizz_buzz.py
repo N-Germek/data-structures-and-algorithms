f029a01f2b778689efd38e7be4bb4cfaf83b71f1
Natalija Germek <n.germek@icloud.com>
2022-12-01 22:08:14 -0800

    added tree-fizz-buzz whiteboard, function buildout, updated README.md and root README.md.

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,35 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.kary_tree import KaryTree
 
 
-def fizz_buzz_tree():
-    pass
+def fizz_buzz_tree(tree):
+    tree = [
+        [1],
+        [2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [10],
+        [11],
+        [12],
+        [13],
+        [14, 15]
+        ]
+    new_tree = []
+    root = KaryTree.root
+    k = 3
+    node = KaryTree.root.value
+    k.value = node.value
+    if new_tree is None:
+        if tree.root.value is not None:
+            KaryTree.breadth_first(tree)
+            if node.value([1]/3 == 1) and node.value([1]/5 == 1):
+                node.value = "FizzBuzz"
+            if node.value([1]/3 ==1) and node.value([1]/5 != 1):
+                node.value = "Fizz"
+            if node.value([1]/5 ==1) and node.value([1]/3 != 1):
+                node.value = "Buzz"
+            else:
+                node.value([1]/3 != 1) and node.value([1]/5 != 1)
+                node.value = str(node.value)
+            new_tree.append(node.value)
+        return new_tree
---
+++ python/docs/tree_fizz_buzz/README.md
@@ -0,0 +1,24 @@
+# Challenge Summary
+Write a function called fizz buzz tree
+Arguments: k-ary tree
+Return: new k-ary tree
+Determine whether or not the value of each node is divisible by 3, 5 or both. Create a new tree with the same structure
+as the original, but the values modified as follows:
+
+If the value is divisible by 3, replace the value with “Fizz”.
+If the value is divisible by 5, replace the value with “Buzz”.
+If the value is divisible by 3 and 5, replace the value with “FizzBuzz”.
+If the value is not divisible by 3 or 5, simply turn the number into a String.
+
+## Whiteboard Process
+[FizzBuzz Tree Whiteboard](/docs/tree_fizz_buzz/tree_fizz_buzz.png)
+[FizzBuzz Tree Function](/code_challenges/tree_fizz_buzz.py)
+
+## Approach & Efficiency
+Version 1.0: December 1, 2022 - Added Whiteboard, created fizz_buzz_tree function, updated README.md and root README.md
+
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## Solution
+
+<!-- Show how to run your code, and examples of it in action -->
