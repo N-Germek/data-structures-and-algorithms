f95b2f64b0eaac9828f7b97f3bc6118472692f6d
Natalija Germek <n.germek@icloud.com>
2023-01-17 00:05:10 -0800

    added code to pycharm.

--- python/code_challenges/hashtable_repeated_word.py
+++ python/code_challenges/hashtable_repeated_word.py
@@ -3,3 +3,67 @@ from data_structures.hashtable import Hashtable
 
 def first_repeated_word():
     pass
+
+
+# str = "Long, long ago, in a land far far away"
+# num_of_chars = 256
+# def count_list(str, count):
+#   for char in str:
+#     count[ord(i)] += 1
+#   return count
+
+# def repeated_word(str):
+#   count = [0] * num_of_chars
+#   count = count_list(str, count)
+
+
+# str = "Long, long ago, in a land far far away"
+
+# def repeated_word(str):
+
+#   string_of_characters = Counter(str)
+#   for letter, count in string_of_characters.items():
+#     if (count > 1):
+#       print(letter)
+
+# repeated_word(str)
+# ---------------------------------------------------------------
+# from collections import Counter
+# import re
+
+# initializing string
+# str = "Long, long ago, in a land far far away"
+#
+#
+# def repeated_word(str):
+    # using regex( findall() )
+    # to extract words from string
+#     words = re.findall(r'\w^+', str)
+#     dict = Counter(words)
+#
+#     for word in words:
+#         if dict[word] > 1:
+#             print(word)
+#
+#
+# repeated_word(str)
+
+----------------------------------------------------------------
+# if str is None:
+
+# 	raise Exception(f'String is Empty')
+
+# else:
+
+# 	for char in str.len():
+
+# 		if isalpha():
+
+# 			string_of_characters.append(char)
+
+# 			for strings in string_of_characters.len:
+
+# 				len(strings) == len(strings) and sorted(strings) == sorted(strings)
+
+# 				return strings
+
